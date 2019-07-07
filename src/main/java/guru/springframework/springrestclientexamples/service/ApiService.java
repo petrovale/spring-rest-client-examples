46a86aca87fdad7e68264ff4cd8073f90bd47956
petrovale <leha.isakov@yandex.ru>
2019-07-07 13:59:48 +0300

    adding webclient example

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiService.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiService.java
@@ -2,8 +2,12 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ApiService {
 
   List<User> getUsers(Integer limit);
+
+  Flux<User> getUsers(Mono<Integer> limit);
 }
